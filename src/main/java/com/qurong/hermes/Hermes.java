d76ec880fd6bb1c92018d90d7aafd0bdaad62589
chen weidong <cwdtom@gmail.com>
2017-12-15 16:37:10 +0800

    bug fix 随机算法选用依据时间戳，保证各服务负载压力平均

--- src/main/java/com/qurong/hermes/Hermes.java
+++ src/main/java/com/qurong/hermes/Hermes.java
@@ -4,6 +4,9 @@ import com.alibaba.fastjson.JSON;
 import com.qurong.hermes.entity.Center;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 服务方法
  *
@@ -21,9 +24,21 @@ public class Hermes {
      * @param data     数据
      * @return 响应结果
      */
-    public String call(String serverId, String name, Object data) throws Exception {
-        String send = JSON.toJSONString(data);
-        Integer index = (int) (Math.random() * this.centers.length);
-        return this.centers[index].call(serverId, name, send);
+    public String call(String serverId, String name, Object data) {
+        // 筛选可用center
+        List<Center> tmp = new ArrayList<>();
+        for (Center c : this.centers) {
+            if (c.getStatus()) {
+                tmp.add(c);
+            }
+        }
+        int index = (int) System.currentTimeMillis() % tmp.size();
+        String resp = null;
+        try {
+            resp = tmp.get(index).call(serverId, name, JSON.toJSONString(data));
+        } catch (Exception ignored) {
+            tmp.get(index).setStatus(false);
+        }
+        return resp;
     }
 }
